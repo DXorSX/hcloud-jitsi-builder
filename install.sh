2f0e802893a9ef4de5f70a5773de9f9b2a9e1a16
aCodingFool <fool@nomail.com>
2020-11-30 22:40:13 +0100

    ..

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-
-
+source ./ENVSETTINGS
 SERVERNAME=test002
-SERVERTYPE=cx31
+SERVERTYPE=ccx31
 SSHPUBKEY=~/.ssh/hetzner_key.pub
 SSHPRIVKEY=~/.ssh/hetzner_key
 GITREPO=https://github.com/DXorSX/hcloud-jitsi-builder.git
@@ -14,7 +13,11 @@ if [ ! -r "$SSHPRIVKEY" ]; then
 	echo "SSH Key does not exist"
 	echo "Generating Key ..."
 	ssh-keygen -t rsa -N "" -f "$SSHPRIVKEY"
+	ssh-add $SSHPRIVKEY
+else
+	ssh-add $SSHPRIVKEY
 fi
+
 
 if [ "$HCCONTEXT" = "" ]; then
 	echo "hcloud context not set, please run: hcloud context create"
@@ -28,19 +31,12 @@ echosyntax ()
 if [ "$SRVSTATUS" = "running" ]; then
 	IPv4=`hcloud server ip $SERVERNAME`
 	echo "Cloud node is still running with ip: $IPv4"
-	#if [ -r "$1" ]; then
-	#	echo "sending NZB to server"
-	#	scp "$1" root@$IPv4:"$NZBREMOTEDROPFOLDER"
-	#	exit	
-	#else
-	#	echo "NZB File needed"
-	#	echosyntax
-	#	exit
-	#fi
+		exit
 fi
 
 hcloud server create --type $SERVERTYPE --name "$SERVERNAME" --image debian-10 --ssh-key $SSHPUBKEY --datacenter fsn1-dc14
 IPv4=`hcloud server ip $SERVERNAME`
+
 
 ssh-keygen -R "$IPv4"
 
@@ -52,7 +48,7 @@ while [ $SSHUP != 0 ]; do
 	SSHUP=$?
 done
 
-ssh -l root "$IPv4" "apt --yes update && \
+ssh -l root "$HOSTNAME" "apt --yes update && \
 	apt --yes install git && \
 	cd /root && \
 	git clone $GITREPO && \
